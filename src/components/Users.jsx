aaf3c42154e475cb086df9c04c57f2f9bf0ef041
Nikex5 <nik.osipenko75@yandex.ru>
2024-04-25 10:50:05 +0300

    commit

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import { Card, Table } from 'antd';
 
 
 function Users() {
@@ -14,20 +14,60 @@ function Users() {
             })
     }, [])
 
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'lastname',
+            dataIndex: 'lastname',
+            key: 'lastname',
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'tg_login',
+            dataIndex: 'tg_login',
+            key: 'tg_login',
+        },
+    ];
+
+
+
     return (
         <>
-            {users.map((item) => (
+
+            {users.map((item) => {
                 <div>
                     {item.id} {item.name} {item.lastname} {item.age} {item.tg_login}
                 </div>
-            ))}
-            {/* //карточки товара */}
-            {users.map((item) => (
-                <div>
-                    {item.id} {item.name} {item.lastname} {item.age} {item.tg_login}
-                </div>
-            ))}
+            })};
+
+            {users.map((itemCard) => {
+
+
+                {/* //карточки товара */ }
+
+                <Card title={itemCard.id} bordered={false}>
+                    {itemCard.name} {itemCard.lastname} {itemCard.age} {itemCard.tg_login}
+                </Card>
+
+
+
+            })}
+
+
+
             {/* таблица */}
+
+            <Table dataSource={users} columns={columns} />;
+
 
         </>
     )
