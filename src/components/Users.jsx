ae84a93a7d7ce6d66d39a822f05775bc3b4a96e0
Nikex5 <nik.osipenko75@yandex.ru>
2024-04-25 11:09:02 +0300

    commit

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -43,25 +43,31 @@ function Users() {
     return (
         <>
             <div><h1>простая отрисовка</h1>
-                {users.map((item) => {
-                    <div>
-                        {item.id} {item.name} {item.lastname} {item.age} {item.tg_login}
-                    </div>
-                })}
+                {users.length > 0 &&
+
+                    users.map((item) => {
+                        <div>
+                            {item.id} {item.name} {item.lastname} {item.age} {item.tg_login}
+                        </div>
+                    })
+                }
             </div>
+
             <div><h1>отрисовка через карточки</h1>
-                {users.map((itemCard) => {
+                {users.length > 0 &&
+
+                    users.map((itemCard) => {
 
 
-                    {/* //карточки товара */ }
+                        {/* //карточки товара */ }
 
-                    <Card title={itemCard.id} bordered={false}>
-                        {itemCard.name} {itemCard.lastname} {itemCard.age} {itemCard.tg_login}
-                    </Card>
+                        <Card title={itemCard.id} bordered={false}>
+                            {itemCard.name} {itemCard.lastname} {itemCard.age} {itemCard.tg_login}
+                        </Card>
 
 
 
-                })}
+                    })}
 
             </div>
 
